b816db775e8069fb58853d229b0d131f23be34ae
asherkhb <asherkhb@gmail.com>
2015-07-31 14:56:55 -0700

    Page dynamics split to separate script

---
+++ demo_pagedynamics.js
@@ -0,0 +1,41 @@
+/**
+ * Created by senorrift on 7/31/15.
+ */
+
+function getBackgroundColor(slideValue) {
+    var colorCodes = ["#f5f5f5", "#dcdcdc", "#c4c4c4", "#ababab", "#939393",
+        "#7a7a7a", "#626262", "#494949", "#313131", "#181818"];
+    return colorCodes[slideValue - 1]
+}
+
+function getHeaderText(dataSelected) {
+    switch (dataSelected) {
+        case "chm":
+            return 'Chicken v. Human v. Mouse';
+            break;
+        case "hco":
+            return "Human v. Chimpanzee v. Orangutan";
+            break;
+        default:
+            return 'Visualization Text Not Specified';
+    }
+}
+
+$(document).ready( function() {
+    /* Change Background Color w/ Slider */
+    var renderDiv = $("#rendering");
+    var bg_slider = $("#bgslider");
+    renderDiv.css('background-color', getBackgroundColor(bg_slider.val()));
+    bg_slider.change( function() {
+        var color = getBackgroundColor(bg_slider.val());
+        renderDiv.css('background-color', color);
+    });
+
+    /* Change Header Description w/ Selection */
+    var headerSpan = $("#current_demo");
+    var selector = $("#dataset_select");
+    headerSpan.html( getHeaderText(selector.val()) );
+    selector.change( function() {
+        headerSpan.html( getHeaderText(selector.val()) );
+    })
+});
